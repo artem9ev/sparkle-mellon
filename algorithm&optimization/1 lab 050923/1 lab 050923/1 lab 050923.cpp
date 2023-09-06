82587e88ebd45e927ac9d2b4e9098d431acfce1a
artem9ev <zemla357@gmail.com>
2023-09-06 22:27:41 +0300

    1lab-02
    added fastCount()

--- algorithm&optimization/1 lab 050923/1 lab 050923/1 lab 050923.cpp
+++ algorithm&optimization/1 lab 050923/1 lab 050923/1 lab 050923.cpp
@@ -2,57 +2,50 @@
 
 using namespace std;
 
+int fastCount(int n) {
+    int count = 0;
+    int* vector = new int[n];
+    int* newVector = new int[n];
+    int** matr = new int* [n];
+    for (int i = 0; i < n; i++)
+    {
+        matr[i] = new int[n];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int k = 0; k < n; k++)
+        {
+            matr[i][k] = 1;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        vector[i] = 1;
+        newVector[i] = 0;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int k = 0; k < n; k++)
+        {
+            newVector[i] += matr[i][k];
+            count += 2;
+        }
+        newVector[i] *= vector[i];
+        count += 2;
+    }
+    return count;
+}
+
 int main()
 {
     int counter[15];
-
-
+    
     setlocale(LC_ALL, "RUS");
     int n = 2;
-    int count = 0;
     for (int c = 0; c < 15; c++, n += 2)
     {
-
-        int* vector = new int[n];
-        int* newVector = new int[n];
-        int** matr = new int* [n];
-        for (int i = 0; i < n; i++)
-        {
-            matr[i] = new int[n];
-        }
-        for (int i = 0; i < n; i++)
-        {
-            for (int k = 0; k < n; k++)
-            {
-                matr[i][k] = 1;
-            }
-        }
-        for (int i = 0; i < n; i++)
-        {
-            vector[i] = 1;
-            newVector[i] = 0;
-        }
-
-        for (int i = 0; i < n; i++)
-        {
-            for (int k = 0; k < n; k++)
-            {
-                newVector[i] += matr[i][k];
-                count += 2;
-            }
-            newVector[i] *= vector[i];
-            count += 2;
-        }
-
-        counter[c] = count;
-        for (int i = 0; i < n; i++)
-        {
-            delete[] matr[i];
-
-        }
-        delete[] matr;
-        delete[] vector;
-        count = 0;
+        counter[c] = fastCount(n);
     }
 
     for (int i = 0; i < 15; i++)
