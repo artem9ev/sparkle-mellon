25bb4ff0b8741ac8fce7d2bc44791b46dc4761df
artem9ev <zemla357@gmail.com>
2023-09-12 18:19:50 +0300

    networking added proj 1

---
+++ networking/simple-client/simple-client/simple-client.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <fstream>
+#include <windows.h>
+using namespace std;
+// структура данных запроса клиента
+struct Person
+{
+	char name[25];  //имя
+	int height;		//рост
+	int weight;		//вес
+} A;
+void main() {
+	setlocale(LC_ALL, "rus");
+
+	const char* nameR = "F:/bread/in-class practice/networking/messages/request.bin"; //файл для запросов клиентов
+	const char* nameA = "F:/bread/in-class practice/networking/messages/answer.bin"; //файл для ответов сервера
+	ofstream f_REQ;
+	ifstream f_ANS;
+	long pred_size;
+	int answer;
+	while (true)
+	{
+		// передача данных от клиента серверу
+		cout << "Введите запрос: Фамилия Рост Вес" << endl;
+		cin >> A.name >> A.height >> A.weight;
+		cout << A.name << A.height << A.weight;
+		f_REQ.open(nameR, ios::app | ios::binary); //открытие файла REQUEST 
+		f_REQ.write((char*)&A, sizeof(A)); //запись запроса в файл REQUEST
+		f_REQ.close();
+
+		// поступил ответ от сервера?
+		f_ANS.open(nameA, ios::binary); //открытие файла ANSWER
+		f_ANS.seekg(0, ios::end); //переход в конец файла ANSWER
+		pred_size = f_ANS.tellg();
+		while (pred_size >= f_ANS.tellg())
+		{
+			Sleep(100); // ждем и переходим в конец файла ANSWER
+			f_ANS.seekg(0, ios::end);
+		}
+		// получение ответа от сервера	
+		f_ANS.seekg(pred_size, ios::beg); // на начало нового ответа
+		f_ANS.read((char*)&answer, sizeof(answer)); //считывание ответа
+		f_ANS.close();
+
+		// расшифровка ответа
+		switch (answer) {
+		case 0: {cout << "Недостаток веса\n"; break; }
+		case 1: {cout << "Норма веса\n"; break; }
+		case 2: {cout << "Избыток веса\n"; break; }
+		}
+	} //while
+}
---
+++ networking/simple-server/simple-server/simple-server.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <fstream>
+#include <windows.h>
+using namespace std;
+struct Person
+{
+	char name[25];	 //имя 
+	int height;		//рост
+	int weight;		//вес
+}B;
+int answer;
+long size_pred;
+int main() {
+	ifstream fR;
+	ofstream fA;
+	setlocale(LC_ALL, "rus");
+	const char* nameR = "F:/bread/in-class practice/networking/messages/request.bin"; //файл запросов клиентов
+	const char* nameA = "F:/bread/in-class practice/networking/messages/answer.bin"; //файл ответов сервера
+	cout << "server is working" << endl;
+	// начальные установки
+	fR.open(nameR, ios::binary); //открытие файла REQUEST
+	fR.seekg(0, ios::end);
+	size_pred = fR.tellg(); //стартовая позиция сервера в файле REQUEST
+	fR.close();
+
+	// начало работы сервера
+	while (true)
+	{
+		fR.open(nameR, ios::binary); //открытие файла REQUEST
+		fR.seekg(0, ios::end); //переход в конец файла REQUEST
+		// есть новые запросы от клиентов? 
+		while (size_pred >= fR.tellg())
+		{
+			Sleep(100); fR.seekg(0, ios::end);
+		}
+		// получен новый запрос 
+		fR.seekg(size_pred, ios::beg); //переход к началу полученного запроса
+		fR.read((char*)&B, sizeof(B)); //считывание данных клиента
+		size_pred = fR.tellg(); // на конец обработанных данных
+		fR.close();
+		// определение индекса массы	
+		double IMT = B.weight / (0.01 * B.height) / (0.01 * B.height);
+		if (18.5 <= IMT && IMT < 25) answer = 1;
+		if (18.5 > IMT) answer = 0;
+		if (IMT >= 25)answer = 2;
+
+		// передача ответа клиенту
+		fA.open(nameA, ios::binary | ios::app); //открытие файла ANSWER
+		fA.write((char*)&answer, sizeof(answer)); //запись ответа клиенту
+		fA.close();
+	}// while
+}
