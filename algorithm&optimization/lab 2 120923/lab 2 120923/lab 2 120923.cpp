9157876804935bd8e4343f27cddf82ea121a5cc0
artem9ev <zemla357@gmail.com>
2023-09-20 06:37:01 +0300

    added a&o lab3

--- algorithm&optimization/lab 2 120923/lab 2 120923/lab 2 120923.cpp
+++ algorithm&optimization/lab 2 120923/lab 2 120923/lab 2 120923.cpp
@@ -6,12 +6,9 @@ using namespace std;
 
 int sortAr(int *a, int n) {
 	int temp, count = 0;
-	for (int i = 0; i < n; i++)
-	{
-		for (int k = 0; k < n; k++)
-		{
-			if (a[i] < a[k])
-			{
+	for (int i = 0; i < n; i++) {
+		for (int k = 0; k < n; k++) {
+			if (a[i] < a[k]) {
 				temp = a[k];
 				a[k] = a[i];
 				a[i] = temp;
@@ -26,8 +23,7 @@ int sortAr(int *a, int n) {
 int createMax(int n) {
 	int value = n;
 	int* a = new int[n];
-	for (int i = 0; i < n; i++)
-	{
+	for (int i = 0; i < n; i++) {
 		a[i] = value--;
 	}
 	return sortAr(a, n);
@@ -35,16 +31,14 @@ int createMax(int n) {
 int createMin(int n) {
 	int value = 0;
 	int* a = new int[n];
-	for (int i = 0; i < n; i++)
-	{
+	for (int i = 0; i < n; i++) {
 		a[i] = value++;
 	}
 	return sortAr(a, n);
 }
 int	createRandom(int n) {
 	int* a = new int[n];
-	for (int i = 0; i < n; i++)
-	{
+	for (int i = 0; i < n; i++) {
 		a[i] = rand();
 	}
 	return sortAr(a, n);
@@ -52,40 +46,28 @@ int	createRandom(int n) {
 
 int main()
 {
-	setlocale(LC_ALL, "ru");
 	srand(time(0));
 
 	int resultMin[10];
 	int resultMax[10];
 	long long resultR[10];
-	for (int i = 0; i < 10; i++)
-	{
+	ofstream file("result.txt");
+
+	for (int i = 0; i < 10; i++) {
 		resultR[i] = 0;
 	}
 	
-	for (int i = 0, n = 100; i < 10; i++, n += 100)
-	{
+	cout << "n:\t\t" << "T-min:\t\t" << "T-max:\t\t" << "T-mean:" << endl;
+	file << "n:\t\t" << "T-min:\t\t" << "T-max:\t\t" << "T-mean:" << endl;
+	for (int i = 0, n = 100; i < 10; i++, n += 100) {
 		resultMin[i] = createMin(n);
 		resultMax[i] = createMax(n);
-		for (int k = 0; k < 1000; k++)
-		{
+		for (int k = 0; k < 1000; k++) {
 			resultR[i] += createRandom(n);
 		}
+		double sred = resultR[i] / 1000.0;
+		cout << fixed << n << "\t\t" << resultMin[i] << "\t\t" << resultMax[i] << "\t\t" << sred << endl;
+		file << fixed << n << "\t\t" << resultMin[i] << "\t\t" << resultMax[i] << "\t\t" << sred << endl;
 	}
-	cout << "T-min: ";
-	for (int i = 0; i < 10; i++)
-	{
-		cout << resultMin[i] << "\t";
-	}
-	cout << "\nT-max: ";
-	for (int i = 0; i < 10; i++)
-	{
-		cout << resultMax[i] << "\t";
-	}
-	cout << "\nT-mean: ";
-	for (int i = 0; i < 10; i++)
-	{
-		resultR[i] /= 1000;
-		cout << resultR[i] << "\t";
-	}
+	file.close();
 }
