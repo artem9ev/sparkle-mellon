ab013fb54d09d576659a7190cc40c78873224f54
artem9ev <zemla357@gmail.com>
2024-03-14 08:26:59 +0300

    backup (140323)

--- low level programming/binom_coef/binom_coef/binom_coef.cpp
+++ low level programming/binom_coef/binom_coef/binom_coef.cpp
@@ -1,56 +1,58 @@
 ﻿#include <iostream>
 
-using namespace std;
-
 int main()
 {
-    int n, k, c = 0;
-    cin >> n >> k;
-    int u, d, g;
-    _asm {
-        mov ECX, k; запихнул в ECX = k
-
-        mov EAX, n
-        mov EBX, k
-        mov u, 1;
-        mov d, 1;
-
-        mov g, ECX
-
-        circle: 
-        CMP ECX, 0
-        JLE circleEnd
-            mov EAX, u
-            mov EBX, n
-            mul EBX
-            mov u, EAX
-
-            mov EAX, d
-            mov EBX, k
-            mul EBX
-            mov d, EAX
+    for (int i = 0; i < 5; i++)
+    {
+        int n, k, c = 0;
+        std::cin >> n >> k;
+        int u, d, g;
+        _asm {
+            mov ECX, k
 
             mov EAX, n
             mov EBX, k
-            sub EAX, 1
-            sub EBX, 1
-            mov n, EAX
-            mov k, EBX
+            mov u, 1;
+            mov d, 1;
 
-            sub ECX, 1
-            JMP circle
-        circleEnd:
+            mov g, ECX
 
-        mov EDX, g
-        mov EAX, u
-        CMP EDX, 0
-        JE exitt
-        mov EDX, 0
-        mov EBX, d
-        div EBX
+                circle :
+            CMP ECX, 0
+                JLE circleEnd
+                mov EAX, u
+                mov EBX, n
+                mul EBX
+                mov u, EAX
 
-        exitt:
-        mov c, EAX
+                mov EAX, d
+                mov EBX, k
+                mul EBX
+                mov d, EAX
+
+                mov EAX, n
+                mov EBX, k
+                sub EAX, 1
+                sub EBX, 1
+                mov n, EAX
+                mov k, EBX
+
+                sub ECX, 1
+                JMP circle
+                circleEnd :
+
+            mov EDX, g
+                mov EAX, u
+                CMP EDX, 0
+                JE exitt
+                mov EDX, 0
+                mov EBX, d
+                div EBX
+
+                exitt :
+            mov c, EAX
+        }
+        std::cout << "c = " << c << std::endl << std::endl;
     }
-    cout << "c = " << c;
+    
 }
---
+++ low level programming/p230306/p230306/p230306.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, n; cin >> x >> n;
+    int i, res = x;
+    _asm {
+        bsr ebx, n
+        mov i, ebx
+
+        
+    }
+    cout << x << "^" << n << " = " << res << " i = " << i;
+
+    /*
+    int x, n; cin >> x >> n;
+    int i, res = x;
+    _asm {
+        xor edx, edx
+        mov eax, type n
+        mov ebx, 8
+        mul ebx
+        mov i, eax
+        
+        mov eax, x
+        mov ebx, n
+        mov ecx, 0
+
+        circlee:
+        cmp ecx, i
+        je exitt
+            xor edx, edx
+            ror n, 1
+            
+            mov eax, n
+            mov ebx, 2
+            div ebx
+            cmp edx, 1
+            xor edx, edx
+                mov eax, res
+                mov ebx, res
+                mul ebx
+                mov res, eax
+            je oneee
+            jmp zeroo
+            oneee:
+                xor edx, edx
+                mov eax, res
+                mov ebx, x
+                mul ebx
+                mov res, eax
+            zeroo:
+            inc ecx
+            jmp circlee
+        exitt:
+ 
+    }
+    cout << x << "^" << n << " = " << res;
+    */
+}
