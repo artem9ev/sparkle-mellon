ffb8bfaf9b68b5d99d2967465306709bbd40bdba
artem9ev <zemla357@gmail.com>
2024-03-18 00:02:09 +0300

    llp 2 & 3 labs

--- low level programming/p240221/p240221/p240221.cpp
+++ low level programming/p240221/p240221/p240221.cpp
@@ -4,61 +4,83 @@ using namespace std;
 
 int main()
 {
-    int a[10];
-	for (int i = 0; i < 10; i++)
+	for (int j = 0; j < 5; j++)
 	{
-		cin >> a[i];
-	}
-	int pos, z;
-	int i, k;
-	_asm {
-		cicleUp:
-		cmp i, 0
-		je endCircleUp
-			circleDown:
-			cmp k, 0
-			je endCircleDown
-
-			endCircleDown:
-		endCircleUp;
-	}
-	for (int i = 0; i < 10; i++)
-	{
-		cout << a[i] << " ";
-	}
-	/*_asm {
-		lea ebx, a
-		mov edx, [ebx]
-		mov z, edx; записал значение 1го элемента
-		add ebx, 4
-		mov ecx, 9
-		mov pos, ebx
-		lea edx, a
-		sub pos, edx
-
-		circleStart: 
-		cmp ecx, 0
-		je circleEnd
+		int a[10];
+		for (int i = 0; i < 10; i++)
+		{
+			cin >> a[i];
+		}
+		int pos, z;
+		int i = 10, k, t;
+		_asm {
+			lea ebx, a
 			mov edx, [ebx]
-		    cmp edx, z
-			jge leavee
+			mov z, edx; записал значение 1го элемента
+			mov pos, ebx
+			lea edx, a
+			sub pos, edx
+			add ebx, 4
+			mov ecx, 9
+
+
+			circleStart :
+			cmp ecx, 0
+				je circleEnd
+				mov edx, [ebx]
+				cmp edx, z
+				jge leavee
 				mov edx, [ebx]
 				mov z, edx
 				mov pos, ebx
 				lea edx, a
 				sub pos, edx
-				leavee: 
+				leavee :
 			add ebx, 4
-		sub ecx, 1
-		jmp circleStart
+				sub ecx, 1
+				jmp circleStart
 
-		circleEnd:
+				circleEnd :
 
-		mov edx, 0
-		mov eax, pos
-		mov bx, 4
-		div bx
-		mov pos, eax		
-	}*/
-	cout << "z: " << z << " pos: " << pos;
+			mov edx, 0
+				mov eax, pos
+				mov bx, 4
+				div bx
+				mov pos, eax
+		}
+		cout << "min: " << z << " pos: " << pos << endl;
+		_asm {
+		circleUp:
+			cmp i, 1
+				je endCircleUp
+				mov k, 10
+				lea ebx, a
+				circleDown :
+			cmp k, 1
+				je endCircleDown
+				mov edx, [ebx]
+				add ebx, 4
+				mov eax, [ebx]
+				cmp edx, eax
+				jle notSwitch
+				mov ecx, eax
+				mov[ebx], edx
+				sub ebx, 4
+				mov[ebx], ecx
+				add ebx, 4
+				notSwitch:
+			sub k, 1
+				jmp circleDown
+				endCircleDown :
+			sub i, 1
+				jmp circleUp
+				endCircleUp :
+		}
+		for (int i = 0; i < 10; i++)
+		{
+			cout << a[i] << " ";
+		}
+		cout << endl << endl;
+	}
+    
 }
---
+++ low level programming/p240228/p230228/p230228.cpp
@@ -0,0 +1,149 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int n;
+    cout << "Размер векторов\nn = "; cin >> n;
+    int u[100];
+    int o[100];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> u[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cin >> o[i];
+    }
+    int i, k, scal = 0;
+    _asm {
+        lea eax, u
+        lea ebx, o
+        mov i, eax
+        mov k, ebx
+        mov ecx, n
+        startt :
+        cmp ecx, 0
+            je exitt
+            xor edx, edx
+            mov edx, i
+            mov eax, [edx]
+            mov edx, k
+            mov ebx, [edx]
+
+            xor edx, edx
+            mul ebx
+            add scal, eax
+            add i, 4
+            add k, 4
+            sub ecx, 1
+            jmp startt
+            exitt :
+    }
+    cout << "Cкалярное произведение = " << scal << endl << endl;
+
+
+    cout << "Размер матриц\nn = "; cin >> n;
+    int a[100][100];
+    int b[100][100];
+    int r[100][100];
+    for (int i = 0; i < n; i++)
+    {
+        for (int k = 0; k < n; k++)
+        {
+            cin >> a[i][k];
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int k = 0; k < n; k++)
+        {
+            cin >> b[i][k];
+        }
+    }
+    int indexx, s, c, aa, bb, g;
+    _asm {
+       mov ecx, n
+       mov s, ecx
+
+       circleString:
+       cmp s, 0
+       je exittt
+           mov c, ecx
+           circleColumn:
+           cmp c, 0
+           je endCircleEnd
+               mov edi, 0
+               mov indexx, ecx
+               circleElem:
+               cmp indexx, 0
+               je endCircleElem
+                   xor edx, edx
+                   mov ebx, indexx
+                   dec ebx
+                   imul eax, ebx, 4
+                   mov g, eax
+                   xor edx, edx
+                   mov ebx, s
+                   dec ebx
+                   imul esi, ebx, 400
+                   add esi, eax 
+
+                   mov eax, a[esi]
+                   mov aa, eax
+
+                   xor edx, edx
+                   mov ebx, c
+                   dec ebx
+                   imul eax, ebx, 4
+                   mov g, eax
+                   xor edx, edx
+                   mov ebx, indexx
+                   dec ebx
+                   imul esi, ebx, 400
+                   add esi, eax
+
+                   mov eax, b[esi]
+                   mov bb, eax
+
+                   xor edx, edx
+                   mov eax, aa
+                   mov ebx, bb
+                   mul ebx
+                   add edi, eax
+
+                   dec indexx
+                   jmp circleElem
+               endCircleElem:
+
+               xor edx, edx
+               mov ebx, c
+               dec ebx
+               imul eax, ebx, 4
+               mov g, eax
+               xor edx, edx
+               mov ebx, s
+               dec ebx
+               imul esi, ebx, 400
+               add esi, eax
+
+               mov r[esi], edi
+
+               dec c
+               jmp circleColumn
+           endCircleEnd:
+           dec s
+           jmp circleString
+       exittt:
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int k = 0; k < n; k++)
+        {
+            cout << " " << r[i][k];
+        }
+        cout << endl;
+    }
+}
