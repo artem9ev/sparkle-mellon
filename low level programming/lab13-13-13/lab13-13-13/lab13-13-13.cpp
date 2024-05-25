48da44fdea7780f3f3717d658441729e66426ec9
artem9ev <zemla357@gmail.com>
2024-05-25 19:47:57 +0300

    31313

---
+++ low level programming/lab13-13-13/lab13-13-13/lab13-13-13.cpp
@@ -0,0 +1,86 @@
+﻿
+#define _SILENCE_AMP_DEPRECATION_WARNINGS
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <amp.h>
+
+using namespace std;
+using namespace std::chrono;
+using namespace concurrency;
+
+
+void MatrixMult_AMP(double* C, const double* A, const double* B, int M, int N, int K) {
+	array_view<const double, 2> a(M, K, A), b(K, N, B);
+	array_view<double, 2> c(M, N, C);
+	c.discard_data();
+	parallel_for_each(c.extent, [=](index<2> indx) restrict(amp)
+		{
+			int row = indx[0];
+			int col = indx[1];
+			double sum = 0;
+			for (int i = 0; i < b.extent[0]; i++)
+			{
+				sum += a(row, i) * b(col, i);
+			}
+			c[indx] = sum;
+		});
+	c.synchronize();
+}
+void MatrixMult(double* C, const double* A, const double* B, int M, int N, int K) {
+	for (int row = 0; row < M; row++)
+	{
+		for (int col = 0; col < N; col++)
+		{
+			double sum = 0;
+			for (int i = 0; i < K; i++)
+			{
+				sum += A[row * K + i] * B[i * N + col];
+			}
+			C[row * N + col] = sum;
+		}
+	}
+}
+
+//int main() {
+//	const int n = 5;
+//	int M, N, K;
+//	M = N = K = n;
+//	double* pa = new double[n * n];
+//	for (int i = 0; i < n * n; i++) {
+//		pa[i] = 1;
+//	}
+//	double* pb = new double[n * n];
+//	for (int i = 0; i < n * n; i++) {
+//		pb[i] = 2;
+//	}
+//	double* pc = new double[n * n];
+//	for (int i = 0; i < n * n; i++) {
+//		pc[i] = 0;
+//	}
+//	auto t0 = high_resolution_clock::now();
+//
+//	MatrixMult_AMP(pc, pa, pb, M, N, K);
+//
+//	auto t1 = high_resolution_clock::now();
+//
+//	MatrixMult(pc, pa, pb, M, N, K);
+//
+//	auto t2 = high_resolution_clock::now();
+//	cout << chrono::duration_cast<milliseconds>(t1 - t0).count() << "msec" << endl;
+//	cout << chrono::duration_cast<milliseconds>(t2 - t1).count() << "msec" << endl;
+//}
+
+int main()
+{
+	vector<accelerator> accelerators = accelerator::get_all();
+	cout << "size: " << accelerators.size() << endl;
+	for (int i = 0; i < accelerators.size(); i++)
+	{
+		wcout << "acc " << accelerators[i].get_description() << endl;
+	}
+	wcout << accelerator(accelerator::default_accelerator).description << endl;
+	wcout << "mem: " << accelerators[0].get_dedicated_memory() << endl;
+	wcout << "path: " << accelerators[0].get_device_path() << endl;
+	wcout << accelerators[0].get_supports_double_precision() << endl;
+}
--- low level programming/lab13/lab13/lab13.cpp
+++ low level programming/lab13/lab13/lab13.cpp
@@ -42,45 +42,45 @@ void MatrixMult(double* C, const double* A, const double* B, int M, int N, int K
 	}
 }
 
-int main() {
-	const int n = 5;
-	int M, N, K;
-	M = N = K = n;
-	double* pa = new double[n * n];
-	for (int i = 0; i < n * n; i++) {
-		pa[i] = 1;
-	}
-	double* pb = new double[n * n];
-	for (int i = 0; i < n * n; i++) {
-		pb[i] = 2;
-	}
-	double* pc = new double[n * n];
-	for (int i = 0; i < n * n; i++) {
-		pc[i] = 0;
-	}
-	auto t0 = high_resolution_clock::now();
-
-	MatrixMult_AMP(pc, pa, pb, M, N, K);
-
-	auto t1 = high_resolution_clock::now();
-
-	MatrixMult(pc, pa, pb, M, N, K);
-
-	auto t2 = high_resolution_clock::now();
-	cout << chrono::duration_cast<milliseconds>(t1 - t0).count() << "msec" << endl;
-	cout << chrono::duration_cast<milliseconds>(t2 - t1).count() << "msec" << endl;
-}
-		
-//int main()
-//{
-//    vector<accelerator> accelerators = accelerator::get_all();
-//    cout << "size: " << accelerators.size() << endl;
-//	for (int i = 0; i < accelerators.size(); i++)
-//	{
-//		wcout << "acc " << accelerators[i].get_description() << endl;
+//int main() {
+//	const int n = 5;
+//	int M, N, K;
+//	M = N = K = n;
+//	double* pa = new double[n * n];
+//	for (int i = 0; i < n * n; i++) {
+//		pa[i] = 1;
 //	}
-//	wcout << accelerator(accelerator::default_accelerator).description << endl;
-//	wcout << "mem: " << accelerators[0].get_dedicated_memory() << endl;
-//	wcout << "path: " << accelerators[0].get_device_path() << endl;
-//	wcout << accelerators[0].get_supports_double_precision() << endl;
+//	double* pb = new double[n * n];
+//	for (int i = 0; i < n * n; i++) {
+//		pb[i] = 2;
+//	}
+//	double* pc = new double[n * n];
+//	for (int i = 0; i < n * n; i++) {
+//		pc[i] = 0;
+//	}
+//	auto t0 = high_resolution_clock::now();
+//
+//	MatrixMult_AMP(pc, pa, pb, M, N, K);
+//
+//	auto t1 = high_resolution_clock::now();
+//
+//	MatrixMult(pc, pa, pb, M, N, K);
+//
+//	auto t2 = high_resolution_clock::now();
+//	cout << chrono::duration_cast<milliseconds>(t1 - t0).count() << "msec" << endl;
+//	cout << chrono::duration_cast<milliseconds>(t2 - t1).count() << "msec" << endl;
 //}
+		
+int main()
+{
+    vector<accelerator> accelerators = accelerator::get_all();
+    cout << "size: " << accelerators.size() << endl;
+	for (int i = 0; i < accelerators.size(); i++)
+	{
+		wcout << "acc " << accelerators[i].get_description() << endl;
+	}
+	wcout << accelerator(accelerator::default_accelerator).description << endl;
+	wcout << "mem: " << accelerators[0].get_dedicated_memory() << endl;
+	wcout << "path: " << accelerators[0].get_device_path() << endl;
+	wcout << accelerators[0].get_supports_double_precision() << endl;
+}
