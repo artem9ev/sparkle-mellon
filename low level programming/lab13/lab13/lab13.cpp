a701feee98ee3beb017c1a7e2f87bbe9571f6f9d
artem9ev <zemla357@gmail.com>
2024-05-25 19:50:20 +0300

    87

--- low level programming/lab13/lab13/lab13.cpp
+++ low level programming/lab13/lab13/lab13.cpp
@@ -10,9 +10,9 @@ using namespace std::chrono;
 using namespace concurrency;
 
 
-void MatrixMult_AMP(double* C, const double* A, const double* B, int M, int N, int K) {
-	array_view<const double, 2> a(M, K, A), b(K, N, B);
-	array_view<double, 2> c(M, N, C);
+void MatrixMult_AMP(double* C, const double* A, const double* B, int n) {
+	array_view<const double, 2> a(n, n, A), b(n, n, B);
+	array_view<double, 2> c(n, n, C);
 	c.discard_data();
 	parallel_for_each(c.extent, [=](index<2> indx) restrict(amp)
 		{
@@ -21,23 +21,43 @@ void MatrixMult_AMP(double* C, const double* A, const double* B, int M, int N, i
 			double sum = 0;
 			for (int i = 0; i < b.extent[0]; i++)
 			{
-				sum += a(row, i) * b(col, i);
+				sum += a(row, i) * b(i, col);
 			}
 			c[indx] = sum;
 		});
 	c.synchronize();
 }
-void MatrixMult(double* C, const double* A, const double* B, int M, int N, int K) {
-	for (int row = 0; row < M; row++)
+
+void MultParall(const double* A, const double* B, double* C, int n) {
+	array_view<const double, 2> a(n, n, A);
+	array_view<const double, 2> b(n, n, B);
+	array_view<double, 2> c(n, n, C);
+	c.discard_data();
+	parallel_for_each(c.extent, [=](index<2> indx) restrict(amp)
+		{
+			int row = indx[0];
+			int col = indx[1];
+			double sum = 0;
+			for (int i = 0; i < b.extent[0]; i++)
+			{
+				sum += a(row, i) * b(i, col);
+				c[indx] = sum;
+			}
+		});
+	c.synchronize();
+}
+
+void MatrixMult(double* C, const double* A, const double* B, int n) {
+	for (int row = 0; row < n; row++)
 	{
-		for (int col = 0; col < N; col++)
+		for (int col = 0; col < n; col++)
 		{
 			double sum = 0;
-			for (int i = 0; i < K; i++)
+			for (int i = 0; i < n; i++)
 			{
-				sum += A[row * K + i] * B[i * N + col];
+				sum += A[row * n + i] * B[i * n + col];
 			}
-			C[row * N + col] = sum;
+			C[row * n + col] = sum;
 		}
 	}
 }
@@ -60,27 +80,13 @@ int main() {
 	}
 	auto t0 = high_resolution_clock::now();
 
-	MatrixMult_AMP(pc, pa, pb, M, N, K);
+	MatrixMult_AMP(pc, pa, pb, n);
 
 	auto t1 = high_resolution_clock::now();
 
-	MatrixMult(pc, pa, pb, M, N, K);
+	MatrixMult(pc, pa, pb, n);
 
 	auto t2 = high_resolution_clock::now();
-	cout << chrono::duration_cast<milliseconds>(t1 - t0).count() << "msec" << endl;
-	cout << chrono::duration_cast<milliseconds>(t2 - t1).count() << "msec" << endl;
+	cout << "GPU: " << chrono::duration_cast<milliseconds>(t1 - t0).count() << "msec" << endl;
+	cout << "CPU: " << chrono::duration_cast<milliseconds>(t2 - t1).count() << "msec" << endl;
 }
-		
-//int main()
-//{
-//    vector<accelerator> accelerators = accelerator::get_all();
-//    cout << "size: " << accelerators.size() << endl;
-//	for (int i = 0; i < accelerators.size(); i++)
-//	{
-//		wcout << "acc " << accelerators[i].get_description() << endl;
-//	}
-//	wcout << accelerator(accelerator::default_accelerator).description << endl;
-//	wcout << "mem: " << accelerators[0].get_dedicated_memory() << endl;
-//	wcout << "path: " << accelerators[0].get_device_path() << endl;
-//	wcout << accelerators[0].get_supports_double_precision() << endl;
-//}
